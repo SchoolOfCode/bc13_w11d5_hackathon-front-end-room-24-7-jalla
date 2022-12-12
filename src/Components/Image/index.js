81261c53ae18830d4a710179e9665abc2a1d457e
Callasaurus <c.sullivandrage@outlook.com>
2022-12-12 11:40:42 +0000

    .

--- src/Components/Image/index.js
+++ src/Components/Image/index.js
@@ -5,6 +5,7 @@ export default function Image(props) {
   
   return (
     <div className='image-containers'>
+      <a href={props.image}>Image link</a>
       <img className='images-for-list' src={props.image} alt="" />
       <button className='delete-button' onClick={props.deleteImageFromDatabase}> Delete image </button>
     </div>
